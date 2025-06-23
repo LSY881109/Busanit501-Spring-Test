b606f0e0e17b0f482c32b0a30e7ea40ab46203a4
이상용 <lsy3709@naver.com>
2025-06-23 15:00:28 +0900

    6교시,Todo 를 활용한 기본 미니 프로젝트1, DTO 이용한 모델 클래스 작업3, 결과를 같지만, 만드는 과정 전,후 빌더 패턴 소개

--- src/main/java/com/busanit501/hello_project/todo/service/TodoService.java
+++ src/main/java/com/busanit501/hello_project/todo/service/TodoService.java
@@ -5,6 +5,8 @@ import com.busanit501.hello_project.todo.dto.TodoDTO;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public enum TodoService {
     // static final 암묵적으로 생략함.
@@ -26,9 +28,22 @@ public enum TodoService {
             todoDTO.setTitle("Todo.." +i);
             todoDTO.setDueDate(LocalDate.now());
         }
+        
+        // 후, 빌더 패턴, 
+        // 연속적인 행위, 
+//        IntStream.range(0,10)
+//        .mapToObj(더미데이터 10개생성)
+//        .collect(생성된 10개 데이터 리스트화)
+       List<TodoDTO> todoDTOList2 = IntStream.range(0,10).mapToObj(i -> {
+            TodoDTO todoDTO = new TodoDTO();
+            todoDTO.setTno((long)i);
+            todoDTO.setTitle("Todo.." +i);
+            todoDTO.setDueDate(LocalDate.now());
+            return todoDTO;
+        }).collect(Collectors.toList());
 
 
-        // 후,
-        return list;
+//        return list;
+        return todoDTOList2;
     }
 }
