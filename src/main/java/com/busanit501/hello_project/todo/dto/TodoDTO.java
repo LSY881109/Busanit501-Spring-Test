f0acf08087da37646877802861634f8bdc4b1984
이상용 <lsy3709@naver.com>
2025-06-23 14:40:27 +0900

    5교시,Todo 를 활용한 기본 미니 프로젝트1, DTO 이용한 모델 클래스 작업3, 서비스 , 등록, 전체조회만 임시로 작성중

--- src/main/java/com/busanit501/hello_project/todo/dto/TodoDTO.java
+++ src/main/java/com/busanit501/hello_project/todo/dto/TodoDTO.java
@@ -61,4 +61,6 @@ public class TodoDTO {
         this.dueDate = dueDate;
         this.finished = finished;
     }
+
+    public TodoDTO() {}
 }
--- src/main/java/com/busanit501/hello_project/todo/service/TodoService.java
+++ src/main/java/com/busanit501/hello_project/todo/service/TodoService.java
@@ -1,5 +1,34 @@
 package com.busanit501.hello_project.todo.service;
 
+import com.busanit501.hello_project.todo.dto.TodoDTO;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public enum TodoService {
+    // static final 암묵적으로 생략함.
     INSTANCE;
+    // 등록 기능,
+    public void register(TodoDTO todoDTO) {
+        System.out.println("서비스에서 작성할 글의 입력 데이터 확인 : "
+                +todoDTO);
+    }
+
+    // 전체 조회,
+    public List<TodoDTO> getList() {
+
+        // 전,
+        List<TodoDTO> list = new ArrayList<>();
+        for (int i =0 ; i <10 ; i++){
+            TodoDTO todoDTO = new TodoDTO();
+            todoDTO.setTno((long)i);
+            todoDTO.setTitle("Todo.." +i);
+            todoDTO.setDueDate(LocalDate.now());
+        }
+
+
+        // 후,
+        return list;
+    }
 }
