41bc806dd355adaff215c22afff19d752c522e51
이상용 <lsy3709@naver.com>
2025-06-24 15:11:15 +0900

    6교시,Hikari CP(Connection Pool), 설정5, dao 패키지 만들고, 등록 기능 추가

--- src/main/java/com/busanit501/hello_project/_3jdbc/dao/TodoDAO.java
+++ src/main/java/com/busanit501/hello_project/_3jdbc/dao/TodoDAO.java
@@ -1,11 +1,9 @@
 package com.busanit501.hello_project._3jdbc.dao;
 
+import com.busanit501.hello_project._3jdbc.domain.TodoVO;
 import lombok.Cleanup;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 
 public class TodoDAO {
     // 1, 테스트용으로 현재 시간 가져오는 메서드 , 디비서버에서 받아오기.
@@ -38,6 +36,27 @@ public class TodoDAO {
         rs.next(); // 가상 테이블에서 데이터 가져오고,
         String now = rs.getString(1);
         return now;
+    } //getTime2
+
+    // 등록 기능. 화면에서 todo의 내용을 전달 받아와서, -> 디비에 넣을 예정.
+    // 화면에서 데이터를 각각 받는 것보다, -> 모델 클래스 담아서 전달.
+    public void insert(TodoVO todoVO) throws Exception{
+        // 등록, insert sql, 문장 작성,
+        String sql ="insert into tbl_todo (title, dueDate, finished) values (?,?,?)";
+
+        // 디비에 연결할 객체 작성, 자원 반납 방법) @Cleanup 사용하기.
+        // 연결 객체, sql 문장 담는 객체 , 반복.
+        @Cleanup Connection connection = ConnectionUtil.INSTANCE.getConnection();
+        @Cleanup PreparedStatement pstmt = connection.prepareStatement(sql);
+
+        pstmt.setString(1, todoVO.getTitle());
+        // 요구사항 Date 타입을 원하는데, LocalDate 타입을 형변환하기.
+        pstmt.setDate(2, Date.valueOf(todoVO.getDueDate()));
+        pstmt.setBoolean(3, todoVO.isFinished());
+
+        // 실제 디비서버에 반영하기. 쓰기 작업 진행.
+        pstmt.executeUpdate();
+
     }
 
 
