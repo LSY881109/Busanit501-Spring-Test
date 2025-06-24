0ad001ee9e014a8139750875a3fcfef9ae1ce7a8
이상용 <lsy3709@naver.com>
2025-06-24 15:20:25 +0900

    6교시,Hikari CP(Connection Pool), 설정5, dao 패키지 만들고, 등록 기능 추가, 단위테스트

--- src/main/java/com/busanit501/hello_project/_3jdbc/domain/TodoVO.java
+++ src/main/java/com/busanit501/hello_project/_3jdbc/domain/TodoVO.java
@@ -2,6 +2,7 @@ package com.busanit501.hello_project._3jdbc.domain;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 import java.time.LocalDate;
@@ -9,6 +10,7 @@ import java.time.LocalDate;
 // 소스코드 생성이 아니라.
 // 롬복 이용해서, 사용해보기.
 @Getter
+//@Setter
 @ToString
 @Builder
 public class TodoVO {
--- src/test/java/com/busanit501/dao/TodoDAOTests.java
+++ src/test/java/com/busanit501/dao/TodoDAOTests.java
@@ -1,10 +1,12 @@
 package com.busanit501.dao;
 
 import com.busanit501.hello_project._3jdbc.dao.TodoDAO;
+import com.busanit501.hello_project._3jdbc.domain.TodoVO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 public class TodoDAOTests {
     // 1, TodoDAO 기능 사용하기, 가져오기.
@@ -24,5 +26,24 @@ public class TodoDAOTests {
     @Test
     public void testTime2() throws Exception {
         System.out.println("testTime : " + todoDAO.getTime2());
+    }
+
+    @Test
+    public void testInsert() throws Exception {
+        // 임시 데이터로 구성, 나중에 화면에서 입력 받은 데이터를 사용할 예정.
+
+        // 기본 인스턴스 생성, 빌더 패턴 사용 전
+//        TodoVO todoVO = new TodoVO();
+//        todoVO.setTitle("샘플 제목");
+//        todoVO.setDueDate(LocalDate.now());
+
+        // 빌더 패턴 사용 후
+        TodoVO todoVO = TodoVO.builder()
+                .title("샘플 제목")
+                .dueDate(LocalDate.now())
+                .build();
+
+        // TodoDAO 기능 이용해서, insert 해보기.
+        todoDAO.insert(todoVO);
     }
 }
