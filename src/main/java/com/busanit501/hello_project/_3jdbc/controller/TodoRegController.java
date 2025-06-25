1cee7191306fc1c11746850f168cd8b97edc3c65
이상용 <lsy3709@naver.com>
2025-06-25 15:22:52 +0900

    6교시, 화면 연결 작업, 컨트롤러에서 , 등록 기능,  2) 등록 기능 처리 2)번 작업

--- src/main/java/com/busanit501/hello_project/_3jdbc/controller/TodoRegController.java
+++ src/main/java/com/busanit501/hello_project/_3jdbc/controller/TodoRegController.java
@@ -1,5 +1,6 @@
 package com.busanit501.hello_project._3jdbc.controller;
 
+import com.busanit501.hello_project._3jdbc.dto.TodoDTO;
 import com.busanit501.hello_project._3jdbc.service.TodoService;
 import lombok.extern.log4j.Log4j2;
 
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 @WebServlet(name = "todoRegController2", value = "/todo/register2")
@@ -24,5 +26,26 @@ public class TodoRegController extends HttpServlet {
         log.info("TodoRegController : 등록 화면 제공 , doGet 작업");
         // 화면 전달 먼저 하기.
         req.getRequestDispatcher("/WEB-INF/todo/todoReg.jsp").forward(req, resp);
+    }
+
+    // 등록 처리 기능
+    // 화면으로부터 전달 받은 데이터를 , 모델 클래스 TodoDTO에 담아서 전달 하기.
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        TodoDTO todoDTO = TodoDTO.builder()
+                .title(req.getParameter("title"))
+                .dueDate(LocalDate.parse(req.getParameter("dueDate"), formatter))
+                .build();
+        log.info("TodoRegController  작업중, doPost 처리중./todo/register2");
+        log.info("전달 받은 데이터 변환 확인 : " + todoDTO);
+
+        // 서비스에 외주 주기, 데이터를 전달만 해주면 됨.
+        try{
+            todoService.register(todoDTO);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // 데이터를 전달 후, 다시, PRG 패턴, post 처리후,  다시 화면으로 전환해주기.
+        resp.sendRedirect("/todo/list2");
     }
 }
