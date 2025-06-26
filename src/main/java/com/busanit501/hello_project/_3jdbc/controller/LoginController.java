dd30a557d7089752a518eaa7c8c052bf3fde1b13
이상용 <lsy3709@naver.com>
2025-06-26 14:55:54 +0900

    6교시, 쿠키,세션 이용해서, 로그인 처리 구현3, 세션에, mid+mpw 더한 문자열을 값으로 저장, 임시 로그인 로직 구현중

--- src/main/java/com/busanit501/hello_project/_3jdbc/controller/LoginController.java
+++ src/main/java/com/busanit501/hello_project/_3jdbc/controller/LoginController.java
@@ -7,6 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.sql.rowset.serial.SerialException;
 import java.io.IOException;
 
@@ -14,8 +15,25 @@ import java.io.IOException;
 @Log4j2
 public class LoginController extends HttpServlet {
     @Override
-    protected  void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         log.info("LoginController, doGet 확인 중.");
         req.getRequestDispatcher("/WEB-INF/login.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log.info("로그인 로직 처리 , doPost");
+        //로그인 화면에서, mid, mpw 의 값을 가져오기,
+        String mid = req.getParameter("mid");
+        String mpw = req.getParameter("mpw");
+
+        String str = mid+mpw;
+
+        // 임시 로그인 구현, 서버의 세션을 이용해서,
+        // 서버의 임시 메모리 공간인 세션이라는 곳에, 키 : loginInfo , 값으로 : mid+mpw, 저장,
+        HttpSession session = req.getSession();
+        // 키 : loginInfo , 값으로 : mid+mpw, 저장
+        session.setAttribute("loginInfo", str);
+        resp.sendRedirect("/todo/list2");
     }
 }
