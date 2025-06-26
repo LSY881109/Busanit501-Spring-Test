8498b2e9fa352f0b5bed2f4c9b646a491ecc0b48
이상용 <lsy3709@naver.com>
2025-06-26 14:32:03 +0900

    5교시, 쿠키,세션 이용해서, 로그인 처리 구현2, 기본 화면과 로그인 컨트롤러 작업

---
+++ src/main/java/com/busanit501/hello_project/_3jdbc/controller/LoginController.java
@@ -0,0 +1,21 @@
+package com.busanit501.hello_project._3jdbc.controller;
+
+import lombok.extern.log4j.Log4j2;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.rowset.serial.SerialException;
+import java.io.IOException;
+
+@WebServlet(name = "loginController", urlPatterns = "/login")
+@Log4j2
+public class LoginController extends HttpServlet {
+    @Override
+    protected  void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log.info("LoginController, doGet 확인 중.");
+        req.getRequestDispatcher("/WEB-INF/login.jsp").forward(req, resp);
+    }
+}
