7d2ad312d581c69157bbd2c7345b0a386e8efe3e
이상용 <lsy3709@naver.com>
2025-06-27 15:40:01 +0900

    6교시, 쿠키,세션 활용해서 자동로그인, 로그인 필터에서, uuid 를 검사해서, 로그인 구현 준비작업

--- src/main/java/com/busanit501/hello_project/_3jdbc/filter/LoginCheckFilter.java
+++ src/main/java/com/busanit501/hello_project/_3jdbc/filter/LoginCheckFilter.java
@@ -4,6 +4,7 @@ import lombok.extern.log4j.Log4j2;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -26,13 +27,33 @@ public class LoginCheckFilter implements Filter {
 
         // 세션에 키 : loginInfo , 값: mid+mpw , 존재하면, 로그인 된 상태이고,
         // 세션에 키 : loginInfo  없으면, 로그인 안되었다.
-        if (session.getAttribute("loginInfo") == null) {
-            log.info("LoginCheckFilter 에서, 작업중, loginInfo 비어 있음");
-            resp.sendRedirect("/login");
-            return;
+
+        // 전
+//        if (session.getAttribute("loginInfo") == null) {
+//            log.info("LoginCheckFilter 에서, 작업중, loginInfo 비어 있음");
+//            resp.sendRedirect("/login");
+//            return;
+//        }
+        //=============================================================
+        // 변경 후,
+        if (session.getAttribute("loginInfo") != null) {
+            // loginInfo 정보가 있다면, 계속 진행하기.(리스트로 가려다가, 필터에
+            // 걸려서 검사 당했음. 다시 리스트로 가기. )
+            filterChain.doFilter(servletRequest, servletResponse);
         }
+        // loginInfo가 없다면, if 아래 코드 실행.
+        // 쿠키를 체크. 쿠키 찾기.
+        Cookie cookie = findCookie(req.getCookies(), "remember-me");
+
+        //=============================================================
 
        // 위의 검사 규칙이 끝나고, 이어서 진행하겠다. 의미라고 보면 됨.
         filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    // todoReadController 에 있는데, 통으로 복사했음. , 나중에 많이 사용시.
+    // 파일 분리해서 사용해도 됩니다.
+    private Cookie findCookie(Cookie[] cookies, String findCookieName) {
+        return  null;
     }
 }
